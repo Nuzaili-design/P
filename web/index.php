be628dcc62c49cd2da64592cde3c98455fc33bf3
Nuzaili-design <abal-nuzaili@stu.okan.edu.tr>
2025-02-23 23:27:14 +0300

    updated

--- web/index.php
+++ web/index.php
@@ -77,50 +77,64 @@ if (isset($_GET['Success'])) {
     <!-- Navbar End -->
 
     <!-- Carousel Start -->
-    <div class="container-fluid p-0 mb-5">
-        <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="w-100" src="img/parkk.jpg" alt="Image">
-                    <div class="carousel-caption d-flex align-items-center">
-                        <div class="container">
-                            <div class="row align-items-center justify-content-center justify-content-lg-start">
-                                <div class="col-10 col-lg-7 text-center text-lg-start">
-                                    <h6 class="text-white text-uppercase mb-3 animated slideInDown">// Vehicle Parking //</h6>
-                                    <h2 class="display-3 text-white mb-4 pb-3 animated slideInDown">BETTER PARKING EXPERIENCE</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img class="w-100" src="img/pexels-k-howard-2220292.jpg" alt="Image">
-                    <div class="carousel-caption d-flex align-items-center">
-                        <div class="container">
-                            <div class="row align-items-center justify-content-center justify-content-lg-start">
-                                <div class="col-10 col-lg-7 text-center text-lg-start">
-                                    <h6 class="text-white text-uppercase mb-3 animated slideInDown">// Vehicle Parking //</h6>
-                                    <h1 class="display-3 text-white mb-4 pb-3 animated slideInDown">QR Code-based Smart Vehicle Parking Management System</h1>
-                                </div>
-                                <div class="col-lg-5 d-none d-lg-flex animated zoomIn">
-                                    <img class="img-fluid" src="img/carousel-2.png" alt="">
-                                </div>
+<div class="container-fluid p-0 mb-5">
+    <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <!-- First Image -->
+            <div class="carousel-item active">
+                <img class="w-100" src="img/parkk.jpg" alt="Image">
+                <div class="carousel-caption d-flex align-items-center">
+                    <div class="container">
+                        <div class="row align-items-center justify-content-center justify-content-lg-start">
+                            <div class="col-10 col-lg-7 text-center text-lg-start">
+                                <h2 class="text-white text-uppercase mb-3 animated slideInDown">fast and reliable way of car parking</h2>
+                                <p class="text-white mb-4 animated slideInDown">Our smart parking system ensures a smooth experience, reducing wait times and maximizing efficiency for drivers.</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#header-carousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+            <!-- Second Image -->
+            <div class="carousel-item">
+                <img class="w-100" src="img/pexels-k-howard-2220292.jpg" alt="Image">
+                <div class="carousel-caption d-flex align-items-center">
+                    <div class="container">
+                        <div class="row align-items-center justify-content-center justify-content-lg-start">
+                            <div class="col-10 col-lg-7 text-center text-lg-start">
+                                <h2 class="text-white text-uppercase mb-3 animated slideInDown">Parking Made Simple, Just for You</h2>
+                                <p class="text-white mb-4 animated slideInDown">Say goodbye to stressful parking! Our reliable service helps you find and secure a spot in no time.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Third Image  -->
+            <div class="carousel-item">
+                <img class="w-100" src="img/egoh.jpg" alt="Image">
+                <div class="carousel-caption d-flex align-items-center">
+                    <div class="container">
+                        <div class="row align-items-center justify-content-center justify-content-lg-start">
+                            <div class="col-10 col-lg-7 text-center text-lg-start">
+                                <h2 class="text-white text-uppercase mb-3 animated slideInDown">Simplifying Your Parking Experience</h2>
+                                <p class="text-white mb-4 animated slideInDown">With our intuitive platform, you'll enjoy easy access to parking spaces, reducing waiting times and offering a seamless service.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#header-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#header-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
-    <!-- Carousel End -->
+</div>
+<!-- Carousel End -->
+
 
     <!-- Service Start -->
     <div class="container-xxl py-5">
