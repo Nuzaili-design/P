f10f5160f88ffd6abfbb3a0e8b713d85984504b2
Nuzaili-design <abal-nuzaili@stu.okan.edu.tr>
2025-03-08 01:40:19 +0300

    Updated

--- web/qrparking (1).sql
+++ web/qrparking (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2025 at 11:25 PM
+-- Generation Time: Mar 07, 2025 at 11:38 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -52,10 +52,10 @@ INSERT INTO `parking_cost` (`id`, `hour`, `cost`) VALUES
 --
 
 CREATE TABLE `parking_details` (
-  `id` int(11) NOT NULL,
+  `id` int(10) NOT NULL,
   `vnumber` varchar(255) NOT NULL,
   `codeval` varchar(255) DEFAULT NULL,
-  `uid` varchar(255) DEFAULT NULL,
+  `uid` int(45) DEFAULT NULL,
   `uname` varchar(255) DEFAULT NULL,
   `slot_name` varchar(255) DEFAULT NULL,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
@@ -69,18 +69,18 @@ CREATE TABLE `parking_details` (
 
 CREATE TABLE `slot_booking` (
   `id` int(10) UNSIGNED NOT NULL,
-  `uname` varchar(45) NOT NULL,
-  `uid` varchar(45) NOT NULL,
+  `uname` varchar(255) NOT NULL,
+  `uid` int(45) NOT NULL,
   `pdate` date NOT NULL,
   `stime` time NOT NULL,
   `phrs` varchar(45) NOT NULL,
   `umail` varchar(45) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL,
-  `slot_name` varchar(45) NOT NULL,
-  `time` varchar(45) NOT NULL,
+  `slot_name` varchar(255) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT current_timestamp(),
   `endtime` time NOT NULL,
   `pcost` varchar(45) NOT NULL,
   `image_data` longblob DEFAULT NULL,
-  `codeval` varchar(45) DEFAULT NULL,
+  `codeval` varchar(255) DEFAULT NULL,
   `ustatus` varchar(45) NOT NULL DEFAULT 'No'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
@@ -89,15 +89,27 @@ CREATE TABLE `slot_booking` (
 --
 
 INSERT INTO `slot_booking` (`id`, `uname`, `uid`, `pdate`, `stime`, `phrs`, `umail`, `slot_name`, `time`, `endtime`, `pcost`, `image_data`, `codeval`, `ustatus`) VALUES
-(85, 'bob', '14', '2025-02-19', '06:00:00', '2', 'bob@gmail.com', 'Slot 1', '6:00', '08:00:00', '100', NULL, NULL, 'No'),
-(86, 'bob', '14', '2025-02-19', '06:00:00', '1', 'bob@gmail.com', 'Slot 1', '6:00', '07:00:00', '50', NULL, NULL, 'No'),
-(87, 'eee', '19', '2025-02-19', '06:00:00', '1', 'eee@gmail.com', 'Slot 2', '06:00:00', '07:00:00', '50', NULL, NULL, 'No'),
-(88, 'eee', '19', '2025-02-19', '11:00:00', '2', 'eee@gmail.com', 'Slot 1', '11:00:00', '13:00:00', '100', NULL, NULL, 'No'),
-(89, 'eee', '19', '2025-02-19', '11:00:00', '2', 'eee@gmail.com', 'Slot 2', '11:00:00', '13:00:00', '100', NULL, NULL, 'No'),
-(90, 'bob', '14', '2025-02-19', '15:00:00', '1', 'bob@gmail.com', 'Slot 1', '15:00:00', '16:00:00', '50', NULL, NULL, 'No'),
-(91, 'bob', '14', '2025-02-19', '10:00:00', '1', 'bob@gmail.com', 'Slot 1', '10:00:00', '11:00:00', '50', NULL, NULL, 'No'),
-(92, 'bob', '14', '2025-02-23', '07:00:00', '1', 'bob@gmail.com', 'Slot 1', '07:00:00', '08:00:00', '50', NULL, NULL, 'No'),
-(93, 'bob', '14', '2025-02-23', '14:00:00', '1', 'bob@gmail.com', 'Slot 1', '14:00:00', '15:00:00', '50', NULL, NULL, 'No');
+(114, 'eee', 19, '2025-02-26', '12:00:00', '1', 'eee@gmail.com', 'Slot 1', '0000-00-00 00:00:00', '13:00:00', '50', '', '', 'No'),
+(115, 'eee', 19, '2025-02-26', '12:00:00', '1', 'eee@gmail.com', 'Slot 2', '0000-00-00 00:00:00', '13:00:00', '50', '', '', 'No'),
+(116, 'bob', 14, '2025-02-26', '12:00:00', '1', 'bob@gmail.com', 'Slot 3', '0000-00-00 00:00:00', '13:00:00', '50', '', '', 'No'),
+(117, 'bob', 14, '2025-02-26', '13:00:00', '2', 'bob@gmail.com', 'Slot 1', '0000-00-00 00:00:00', '15:00:00', '100', '', '', 'No'),
+(118, 'eee', 19, '2025-02-26', '15:00:00', '1', 'eee@gmail.com', 'Slot 1', '0000-00-00 00:00:00', '16:00:00', '50', '', '', 'No'),
+(119, 'eee', 19, '2025-03-01', '06:00:00', '2', 'eee@gmail.com', 'Slot 1', '0000-00-00 00:00:00', '08:00:00', '100', '', '', 'No'),
+(120, 'bob', 14, '2025-03-01', '11:00:00', '1', 'bob@gmail.com', 'Slot 1', '0000-00-00 00:00:00', '12:00:00', '50', '', '', 'No'),
+(121, 'bob', 14, '2025-03-01', '11:00:00', '1', 'bob@gmail.com', 'Slot 2', '0000-00-00 00:00:00', '12:00:00', '50', '', '', 'No'),
+(122, 'bob', 14, '2025-03-01', '11:00:00', '1', 'bob@gmail.com', 'Slot 3', '0000-00-00 00:00:00', '12:00:00', '50', '', '', 'No'),
+(123, 'bob', 14, '2025-03-01', '11:00:00', '1', 'bob@gmail.com', 'Slot 4', '2025-03-01 19:07:07', '12:00:00', '50', '', '', 'No'),
+(124, 'bob', 14, '2025-03-01', '11:00:00', '1', 'bob@gmail.com', 'Slot 5', '2025-03-01 19:10:49', '12:00:00', '50', '', '', 'No'),
+(125, 'eee', 19, '2025-03-01', '15:00:00', '2', 'eee@gmail.com', 'Slot 1', '2025-03-01 19:16:09', '17:00:00', '100', '', '', 'No'),
+(126, 'faf', 21, '2025-03-01', '11:00:00', '1', 'faf@gmail.com', 'Slot 6', '2025-03-01 19:27:35', '12:00:00', '50', '', '', 'No'),
+(127, 'eee', 19, '2025-03-07', '16:00:00', '1', 'eee@gmail.com', 'Slot 1', '2025-03-07 17:33:15', '17:00:00', '50', '', '', 'No'),
+(128, 'bob', 14, '2025-03-07', '16:00:00', '1', 'bob@gmail.com', 'Slot 2', '2025-03-07 17:59:32', '17:00:00', '50', 0x89504e470d0a1a0a0000000d4948445200000091000000910103000000f916c86000000006504c5445ffffff00000055c2d37e000000097048597300000ec400000ec401952b0e1b000000d0494441544889ed94310ac3301004175ca4d413f413e7630109f432ff444f70a9c26473273bc4b1db3b70e12d4ed2348b96e5805b175320f9c650db532ecd89656094b3f5e1c40adb184ac518583d59f7f566afe8ca7a7e8938656ac8b40792dfa91b866cfb60c551864c5213c3527ba99d187a6a898f694eb539b1a496c238237a3180933c3931ef22b46583588ad4f7db3573a65a4b3dd089ad7dce621996e8c5d4322c7d594638b16d4fcaf8f97a30b1d43e138e4cc6be07e6ace757287d5efe333564eb9e54df43d7ecd8ad0be9039c85af40f2e1c5500000000049454e44ae426082, '', 'No'),
+(129, 'bob', 14, '2025-03-07', '14:00:00', '2', 'bob@gmail.com', 'Slot 1', '2025-03-07 19:50:15', '16:00:00', '100', '', '', 'No'),
+(130, 'bob', 14, '2025-03-07', '14:00:00', '1', 'bob@gmail.com', 'Slot 2', '2025-03-07 19:53:58', '15:00:00', '50', '', '', 'No'),
+(131, 'bob', 14, '2025-03-07', '06:00:00', '1', 'bob@gmail.com', 'Slot 7', '2025-03-07 20:27:27', '07:00:00', '50', '', '', 'No'),
+(132, 'eee', 19, '2025-03-08', '14:00:00', '1', 'eee@gmail.com', 'Slot 1', '2025-03-07 21:39:33', '15:00:00', '50', '', '', 'No'),
+(133, 'eee', 19, '2025-03-08', '14:00:00', '1', 'eee@gmail.com', 'Slot 2', '2025-03-07 21:48:07', '15:00:00', '50', NULL, NULL, 'No'),
+(134, 'eee', 19, '2025-03-08', '14:00:00', '1', 'eee@gmail.com', 'Slot 3', '2025-03-07 21:56:24', '15:00:00', '50', NULL, NULL, 'No');
 
 -- --------------------------------------------------------
 
@@ -178,19 +190,19 @@ ALTER TABLE `parking_cost`
 -- AUTO_INCREMENT for table `parking_details`
 --
 ALTER TABLE `parking_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `slot_booking`
 --
 ALTER TABLE `slot_booking`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=135;
 
 --
 -- AUTO_INCREMENT for table `user_reg`
 --
 ALTER TABLE `user_reg`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
